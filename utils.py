fd95a17a8f73b6cad256d0955da902d1c818efd7
sayZeeel <es16btech11005@iith.ac.in>
2020-05-11 03:14:04 +0530

    define necessary functions here

---
+++ utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import h5py
+from keras.models import load_model
+import keras
+import keras.backend as K 
+from sklearn.metrics import classification_report, confusion_matrix
+
+def get_snr_range():
+	return np.array([-20,-18,-16,-14,-12,-10,-8,-6,-4,-2,0,2,4,6,8,10,12,14,16,18])
+
+def evaluate_model(models_dir,modelfile,xtest,ytest,test_snr_sequence):
+	m = load_model(models_dir+modelfile)
+	ypred = m.predict(xtest)
+	SNR = get_snr_range()
+
+	val_acc = []
+
+
+	for snr in SNR:
+		p = np.where(test_snr_sequence == snr)[0]
+		y_gt = ytest[p]
+		y_pd = ypred[p]
+
+		gt_class =np.argmax(np.array(y_gt),axis = 1)
+		pd_class =np.argmax(np.array(y_pd),axis = 1)
+
+		acc = len(np.where(gt_class==pd_class)[0])*100/len(gt_class)
+		val_acc.append(acc)
+
+		print("===============================================================\n")
+		print("SNR: {}dB, Validation Accuracy: {}\n".format(snr,acc))
+		print("\n             Classification Report")
+		print(classification_report(gt_class,pd_class))
+		print("\n             Confusion Matrix")
+		c = np.array(confusion_matrix(gt_class,pd_class))
+		# c = np.round(c*100/len(gt_class),2)
+		print(c)
+
+	plt.plot(SNR,val_acc,label = 'validation_accuracy')
+	plt.title('Accuracy vs SNRs')
+	plt.xlabel('Accuracy')
+	plt.xlabel('SNRs')
+	plt.legend()
+	plt.grid(True)
+	plt.savefig(models_dir + 'fig_val_acc.png')

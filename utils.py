07fca6f39ec449273a7703e653095da5b10a80b5
sayZeeel <es16btech11005@iith.ac.in>
2020-05-11 10:22:45 +0530

    add baseline papers and modified utils

--- utils.py
+++ utils.py
@@ -10,7 +10,17 @@ from sklearn.metrics import classification_report, confusion_matrix
 def get_snr_range():
 	return np.array([-20,-18,-16,-14,-12,-10,-8,-6,-4,-2,0,2,4,6,8,10,12,14,16,18])
 
-def evaluate_model(models_dir,modelfile,xtest,ytest,test_snr_sequence):
+def plot_snr_evaluation(fig_file,SNR_range,val_accuracy):
+
+	plt.plot(SNR_range,val_accuracy,label = 'validation_accuracy')
+	plt.title('Accuracy vs SNRs')
+	plt.xlabel('Accuracy')
+	plt.xlabel('SNRs')
+	plt.legend()
+	plt.grid(True)
+	plt.savefig(fig_file)
+
+def evaluate_model(models_dir,modelfile,xtest,ytest,test_snr_sequence,plot=True):
 	m = load_model(models_dir+modelfile)
 	ypred = m.predict(xtest)
 	SNR = get_snr_range()
@@ -38,10 +48,7 @@ def evaluate_model(models_dir,modelfile,xtest,ytest,test_snr_sequence):
 		# c = np.round(c*100/len(gt_class),2)
 		print(c)
 
-	plt.plot(SNR,val_acc,label = 'validation_accuracy')
-	plt.title('Accuracy vs SNRs')
-	plt.xlabel('Accuracy')
-	plt.xlabel('SNRs')
-	plt.legend()
-	plt.grid(True)
-	plt.savefig(models_dir + 'fig_val_acc.png')
+	if plot == True:
+		plot_snr_evaluation(models_dir + 'fig_val_acc.png',SNR,val_acc)
+
+	return val_acc

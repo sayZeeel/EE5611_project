fcf4bfe7f411525914483b6a6074cf8584bf88ba
v-squared99 <47570498+v-squared99@users.noreply.github.com>
2020-05-22 10:30:11 +0530

    Update model_architectures.py

--- model_architectures.py
+++ model_architectures.py
@@ -12,9 +12,23 @@ def baseline_lstm():
 	l = BatchNormalization()(inputs)
 	l = LSTM(128,return_sequences=True,activation='tanh',unroll=True)(l)   
 	l = LSTM(128,return_sequences=False,activation='tanh',unroll=True)(l)
+	l = Dropout(0.2)(l)	
 	outputs = Dense(11,activation='softmax',kernel_constraint = max_norm(2.))(l)
+	
+	model = Model(inputs,outputs)
+	model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001) ,metrics=['accuracy'])
+	model.summary()
+	return model
+
+def new_lstm():
+	
+	inputs = Input((128,4,))
+	l = BatchNormalization()(inputs)
+	l = LSTM(128,return_sequences=True,activation='tanh',unroll=True)(l)   
+	l = LSTM(128,return_sequences=False,activation='tanh',unroll=True)(l)
 	l = Dropout(0.2)(l)
-    
+	outputs = Dense(11,activation='softmax',kernel_constraint = max_norm(2.))(l)
+
 	model = Model(inputs,outputs)
 	model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001) ,metrics=['accuracy'])
 	model.summary()
@@ -38,6 +52,27 @@ def baseline_conv():
 	model.summary()
 	return model
   
+
+def base_scrnn():
+	
+	inputs = Input((128,2,))
+	l = BatchNormalization()(inputs)
+	# l = Lambda(lambda t: K.expand_dims(t, -2))(l)
+	l = Conv1D(filters=128,kernel_size=5,activation='relu')(l)
+	l = MaxPool1D(3)(l)
+	l = Conv1D(filters=128,kernel_size=5,activation='relu')(l)
+	# l = Lambda(lambda t: K.squeeze(t, -2))(l)
+	l = LSTM(128,return_sequences=True,activation='tanh',unroll=True)(l)    
+	l = LSTM(128,return_sequences=True,activation='tanh',unroll=True)(l)
+	l = Dropout(0.5)(l)     
+	l = Flatten()(l)    
+	outputs = Dense(11,activation='softmax',kernel_constraint = max_norm(2.))(l)
+
+	model = Model(inputs,outputs)
+	model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001) ,metrics=['accuracy'])
+	model.summary()
+	return model
+
 def new_scrnn():
 	
 	inputs = Input((128,4,))

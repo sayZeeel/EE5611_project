715ba42dd5079ef323a9d0f50a6d16283b5c075e
v-squared99 <47570498+v-squared99@users.noreply.github.com>
2020-05-22 11:10:17 +0530

    Update model_architectures.py

--- model_architectures.py
+++ model_architectures.py
@@ -62,6 +62,26 @@ def base_scrnn():
 	l = MaxPool1D(3)(l)
 	l = Conv1D(filters=128,kernel_size=5,activation='relu')(l)
 	# l = Lambda(lambda t: K.squeeze(t, -2))(l)
+	l = LSTM(128,return_sequences=True,activation='relu',unroll=True)(l)    
+	l = LSTM(128,return_sequences=True,activation='relu',unroll=True)(l)
+	l = Dropout(0.8)(l)     
+	l = Flatten()(l)    
+	outputs = Dense(11,activation='softmax',kernel_constraint = max_norm(2.))(l)
+
+	model = Model(inputs,outputs)
+	model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001) ,metrics=['accuracy'])
+	model.summary()
+	return model
+
+def scrnn_tanh():
+	
+	inputs = Input((128,2,))
+	l = BatchNormalization()(inputs)
+	# l = Lambda(lambda t: K.expand_dims(t, -2))(l)
+	l = Conv1D(filters=128,kernel_size=5,activation='relu')(l)
+	l = MaxPool1D(3)(l)
+	l = Conv1D(filters=128,kernel_size=5,activation='relu')(l)
+	# l = Lambda(lambda t: K.squeeze(t, -2))(l)
 	l = LSTM(128,return_sequences=True,activation='tanh',unroll=True)(l)    
 	l = LSTM(128,return_sequences=True,activation='tanh',unroll=True)(l)
 	l = Dropout(0.5)(l)     
